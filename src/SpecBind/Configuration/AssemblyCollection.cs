ba8656b1ee78a2456f4cc8dc4896a3804c9767e6
icnocop <icnocop@users.noreply.github.com>
2018-09-15 02:17:09 -0700

    Fixed "ExcludedAssemblies" configuration collection Fixes #174

--- src/SpecBind/Configuration/AssemblyCollection.cs
+++ src/SpecBind/Configuration/AssemblyCollection.cs
@@ -3,39 +3,34 @@
 // </copyright>
 namespace SpecBind.Configuration
 {
-	using System.Collections.Generic;
-	using System.Configuration;
+    using System.Configuration;
 
-	/// <summary>
-	/// Configuration collection for holding assembly information.
-	/// </summary>
-	public class AssemblyCollection : ConfigurationElementCollection
-	{
-		private readonly List<AssemblyElement> assemblies = new List<AssemblyElement>();
+    /// <summary>
+    /// Configuration collection for holding assembly information.
+    /// </summary>
+    public class AssemblyCollection : ConfigurationElementCollection
+    {
+        /// <summary>
+        /// When overridden in a derived class, creates a new <see cref="T:System.Configuration.ConfigurationElement"/>.
+        /// </summary>
+        /// <returns>
+        /// A new <see cref="T:System.Configuration.ConfigurationElement"/>.
+        /// </returns>
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new AssemblyElement();
+        }
 
-		/// <summary>
-		/// When overridden in a derived class, creates a new <see cref="T:System.Configuration.ConfigurationElement"/>.
-		/// </summary>
-		/// <returns>
-		/// A new <see cref="T:System.Configuration.ConfigurationElement"/>.
-		/// </returns>
-		protected override ConfigurationElement CreateNewElement()
-		{
-			var newAssembly = new AssemblyElement();
-			this.assemblies.Add(newAssembly);
-			return newAssembly;
-		}
-
-		/// <summary>
-		/// Gets the element key for a specified configuration element when overridden in a derived class.
-		/// </summary>
-		/// <returns>
-		/// An <see cref="T:System.Object"/> that acts as the key for the specified <see cref="T:System.Configuration.ConfigurationElement"/>.
-		/// </returns>
-		/// <param name="element">The <see cref="T:System.Configuration.ConfigurationElement"/> to return the key for. </param>
-		protected override object GetElementKey(ConfigurationElement element)
-		{
-			return this.assemblies.Find(a => a.Equals(element));
-		}
-	}
+        /// <summary>
+        /// Gets the element key for a specified configuration element when overridden in a derived class.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Object"/> that acts as the key for the specified <see cref="T:System.Configuration.ConfigurationElement"/>.
+        /// </returns>
+        /// <param name="element">The <see cref="T:System.Configuration.ConfigurationElement"/> to return the key for. </param>
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((AssemblyElement)element).Name;
+        }
+    }
 }
